40f6bae73d28aff697b9a6134f0e01a5f32458bf
Brian Carr <brian.carr@onassignment.com>
2012-11-01 17:37:47 -0700

    Added basic configurations and default help event.

--- examples/simple_program.js
+++ examples/simple_program.js
@@ -1,6 +1,9 @@
+/**
+ * Run this program with "node simple_program.js"
+ */
 var repl = require('../lib/replcator').getInstance();
 
-repl.on('echo {text}', function(repl) {
+repl.on('echo {text:.*}', function(repl) {
     console.log("You said: %s", repl.getAttr('text'));
 });
 
